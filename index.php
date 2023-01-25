4b1e3bc7508714301ae398c44223e846b99418c0
Aleksey08kk <93997625+Aleksey08kk@users.noreply.github.com>
2023-01-25 18:25:23 +0400

    Был поиск подключения Docker для дальнейшей работы.

---
+++ index.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <title>Авторизация</title>
+</head>
+<body>
+
+    <form action="" method="">
+        <label>Логин</label>
+        <input type="text">
+        <label>Пароль</label>
+        <input type="text">
+        <button>Войти</button>
+    </form>
+
+</body>
+</html>
