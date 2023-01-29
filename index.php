f1514bc073d882bde8103a645172e9d362145232
Aleksey08kk <93997625+Aleksey08kk@users.noreply.github.com>
2023-01-29 12:17:11 +0400

    Создал подключение к БД

--- index.php
+++ index.php
@@ -2,16 +2,18 @@
 <html lang="ru">
 <head>
     <meta charset="UTF-8">
+    <link rel="stylesheet" href="index.css">
     <title>Авторизация</title>
 </head>
 <body>
 
     <form action="" method="">
         <label>Логин</label>
-        <input type="text">
+        <input type="text" placeholder="Введите логин">
         <label>Пароль</label>
-        <input type="text">
+        <input type="text" placeholder="Введите пароль">
         <button>Войти</button>
+        <p>Нет аккаунта? - <a href="register.php">зарегистрируйтесь</a></p>
     </form>
 
 </body>
